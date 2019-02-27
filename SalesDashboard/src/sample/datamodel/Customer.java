b18df46e5aac1c5d9554c7f595a5698a6e6af4af
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-02-27 22:44:04 +0000

    Initial commit

---
+++ SalesDashboard/src/sample/datamodel/Customer.java
@@ -0,0 +1,61 @@
+package sample.datamodel;
+
+import javafx.beans.property.SimpleStringProperty;
+
+import java.time.LocalDate;
+
+/**
+ * Created by Johnny on 27/02/2019
+ */
+public class Customer {
+
+    private SimpleStringProperty firstName;
+    private SimpleStringProperty lastName;
+    private SimpleStringProperty email;
+    private SimpleStringProperty phoneNum;
+
+    public Customer() {
+    }
+
+    public Customer(String firstName, String lastName, String email, String phoneNum, LocalDate dateLastContacted) {
+        this.firstName = new SimpleStringProperty(firstName);
+        this.lastName = new SimpleStringProperty(lastName);
+        this.email = new SimpleStringProperty(email);
+        this.phoneNum = new SimpleStringProperty(phoneNum);
+
+    }
+
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
+
+    public String getPhoneNum() {
+        return phoneNum.get();
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum.set(phoneNum);
+    }
+
+
+}
