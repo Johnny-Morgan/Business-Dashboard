8e7e3f9c8fa358dcc1f722719ba0db05c5fe22f1
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2019-04-25 13:41:58 +0100

    Added listContextMenu to ExpenseController

--- SalesDashboard/src/sample/ExpenseController.java
+++ SalesDashboard/src/sample/ExpenseController.java
@@ -2,6 +2,8 @@ package sample;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Side;
@@ -13,12 +15,14 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import sample.datamodel.Customer;
+import javafx.stage.WindowEvent;
 import sample.datamodel.Expense;
 import sample.datamodel.ExpenseData;
 
 import java.io.IOException;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ExpenseController {
 
@@ -29,13 +33,14 @@ public class ExpenseController {
     private Button editButton;
     @FXML
     private Button deleteButton;
-
     @FXML
     private TableView<Expense> expensesTable;
+    @FXML
+    private ContextMenu listContextMenu;
     private ExpenseData data;
 
     private final ObservableList<PieChart.Data> details = FXCollections.observableArrayList();
-    private javafx.scene.chart.PieChart pieChart;
+    private PieChart pieChart;
     private BorderPane root;
 
     public void initialize(){
@@ -44,6 +49,16 @@ public class ExpenseController {
         expensesTable.setItems(data.getExpenses());
         editButton.setDisable(true);
         deleteButton.setDisable(true);
+
+        listContextMenu = new ContextMenu();
+        MenuItem deleteMenuItem = new MenuItem("Delete");
+        MenuItem editMenuItem = new MenuItem("Edit");
+
+        deleteMenuItem.setOnAction((ActionEvent event) -> deleteExpense());
+        editMenuItem.setOnAction((ActionEvent event) -> showEditExpenseDialog());
+
+        listContextMenu.getItems().addAll(deleteMenuItem, editMenuItem);
+        expensesTable.setContextMenu(listContextMenu);
     }
 
     public void enableButtons() {
@@ -72,18 +87,37 @@ public class ExpenseController {
         dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
 
         Optional<ButtonType> result = dialog.showAndWait();
+
         if(result.isPresent() && result.get() == ButtonType.OK){
             ExpenseDialogController expenseDialogController = fxmlLoader.getController();
             Expense newExpense = expenseDialogController.getNewExpense();
-            data.addExpense(newExpense);
-            data.saveExpenses();
+
+            boolean check = false;
+            while (!check) {
+                check = validateFields(newExpense) && validateDate(newExpense) && validateAmount(newExpense);
+                if (check) {
+                    data.addExpense(newExpense);
+                    data.saveExpenses();
+                    break;
+                } else {
+                    expenseDialogController = fxmlLoader.getController();
+                    expenseDialogController.editExpense(newExpense);
+
+                    result = dialog.showAndWait();
+
+                    if (result.isPresent() && result.get() == ButtonType.OK) {
+                        expenseDialogController.updateExpense(newExpense);
+                        data.saveExpenses();
+                    } else return;
+                }
+            }
         }
     }
 
     @FXML
-    public void showEditExpenseDialog(){
+    public void showEditExpenseDialog() {
         Expense selectedExpense = expensesTable.getSelectionModel().getSelectedItem();
-        if(selectedExpense == null){
+        if (selectedExpense == null) {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setTitle("No Expense Selected");
             alert.setHeaderText(null);
@@ -97,9 +131,9 @@ public class ExpenseController {
         dialog.setTitle("Edit Expense");
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getClass().getResource("expensedialog.fxml"));
-        try{
+        try {
             dialog.getDialogPane().setContent(fxmlLoader.load());
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println("Couldn't load the dialog");
             e.printStackTrace();
             return;
@@ -112,9 +146,26 @@ public class ExpenseController {
         expenseDialogController.editExpense(selectedExpense);
 
         Optional<ButtonType> result = dialog.showAndWait();
-        if(result.isPresent() && result.get() == ButtonType.OK) {
+        if (result.isPresent() && result.get() == ButtonType.OK) {
             expenseDialogController.updateExpense(selectedExpense);
-            data.saveExpenses();
+            boolean check = false;
+            while (!check) {
+                check = validateFields(selectedExpense) && validateDate(selectedExpense) && validateAmount(selectedExpense);
+                if (check) {
+                    data.saveExpenses();
+                    break;
+                } else {
+                    expenseDialogController = fxmlLoader.getController();
+                    expenseDialogController.editExpense(selectedExpense);
+
+                    result = dialog.showAndWait();
+
+                    if (result.isPresent() && result.get() == ButtonType.OK) {
+                        expenseDialogController.updateExpense(selectedExpense);
+                        data.saveExpenses();
+                    } else return;
+                }
+            }
         }
     }
 
@@ -166,15 +217,23 @@ public class ExpenseController {
     public void showPieChart(){
         Stage pieStage = new Stage();
 
-        // hardcoded expense data
-        details.addAll(new javafx.scene.chart.PieChart.Data("Motor", 1440),
-                new javafx.scene.chart.PieChart.Data("Advertising", 1200),
-                new javafx.scene.chart.PieChart.Data("Stationary & Postage", 461),
-                new javafx.scene.chart.PieChart.Data("Telephone & Internet", 720),
-                new javafx.scene.chart.PieChart.Data("Heat & Light", 343),
-                new javafx.scene.chart.PieChart.Data("Meetings & Events", 755),
-                new javafx.scene.chart.PieChart.Data("Bank Charges", 300),
-                new javafx.scene.chart.PieChart.Data("Accountancy Fees", 489));
+        double advertisingExpenses = ExpenseData.getPieDataAdvertising();
+        double motorExpenses = ExpenseData.getPieDataMotor();
+        double phoneExpenses = ExpenseData.getPieDataPhone();
+        double heatExpenses = ExpenseData.getPieDataHeat();
+        double travelExpenses = ExpenseData.getPieDataTravel();
+        double postageExpenses = ExpenseData.getPieDataPostage();
+        double bankExpenses = ExpenseData.getPieDataBank();
+        double miscExpenses = ExpenseData.getPieDataMisc();
+
+        details.addAll(new javafx.scene.chart.PieChart.Data("Advertising", advertisingExpenses),
+                new javafx.scene.chart.PieChart.Data("Motor", motorExpenses),
+                new javafx.scene.chart.PieChart.Data("Stationary & Postage", postageExpenses),
+                new javafx.scene.chart.PieChart.Data("Phone & Broadband", phoneExpenses),
+                new javafx.scene.chart.PieChart.Data("Heat & Light", heatExpenses),
+                new javafx.scene.chart.PieChart.Data("Travel & Subsidence", travelExpenses),
+                new javafx.scene.chart.PieChart.Data("Banking", bankExpenses),
+                new javafx.scene.chart.PieChart.Data("Miscellaneous", miscExpenses));
 
         pieChart = new javafx.scene.chart.PieChart();
         pieChart.setData(details);
@@ -187,5 +246,54 @@ public class ExpenseController {
         root.setCenter(pieChart);
         pieStage.setScene(new Scene(root, 600 , 500));
         pieStage.show();
+
+        pieStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent we) {
+                details.clear();
+            }
+        });
+    }
+
+    // check to see if fields are empty
+    public boolean validateFields(Expense expense) {
+
+        if (expense.getDescription().trim().isEmpty() || expense.getCategory().trim().isEmpty() ||
+                expense.getAmount().trim().isEmpty() || expense.getDate().trim().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Invalid info");
+            alert.setHeaderText(null);
+            alert.setContentText("Invalid info.\nAll fields must be filled.");
+            alert.showAndWait();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validateDate(Expense expense) {
+        if ((!expense.getDate().equals(null))) {
+            return true;
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Invalid Info");
+            alert.setHeaderText(null);
+            alert.setContentText("No Date Entered.\nPlease Enter Date.");
+            alert.showAndWait();
+            return false;
+        }
+    }
+
+    public boolean validateAmount(Expense expense) {
+        Pattern p = Pattern.compile("[0-9.]+");
+        Matcher m = p.matcher(expense.getAmount());
+        if (m.find() && m.group().equals(expense.getAmount())) {
+            return true;
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Invalid Info");
+            alert.setHeaderText(null);
+            alert.setContentText("Please Enter Valid Expense Amount");
+            alert.showAndWait();
+            return false;
+        }
     }
 }
